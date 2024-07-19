c8b3e2e619500a58f834984749ff9825e23917d3
Bilal4509021 <93261313+Bilal4509021@users.noreply.github.com>
2024-07-19 05:43:13 +0330

    Update config.js

--- config.js
+++ config.js
@@ -5,17 +5,17 @@ module.exports = {
     /**
      * bot details and parameters
      */
-    botname: process.env.BotName || "Queen Anya Bot",
-    footer: process.env.Footer || "© Queen Anya Bot",
+    botname: process.env.BotName || "BILAL 9021 Bot",
+    footer: process.env.Footer || "© BILAL 9021 Bot",
     prefa: process.env.Prefix || "-",
     themeemoji: process.env.ThemeEmoji || "🎐",
 
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
-    instagramId: process.env.Insta || "3.69_pika",
+    ownername: process.env.Owner_Name || "BILAL;9021",
+    ownernumber: process.env.Owner_Number || "989364509021",
+    instagramId: process.env.Insta || "bilalsh196_",
 
     /**
      * other details and parameters
@@ -23,11 +23,11 @@ module.exports = {
     author: process.env.Author || "@PikaBotz",
     packname: process.env.PackName || "Queen Anya v2 MD",
     socialLink: process.env.Web || "https://github.com/PikaBotz",
-    groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
+    groupLink: process.env.GcLink || "https://wa.me/+989364509021",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: 5, // default cooldown time per command in seconds
     mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    sessionId: process.env.SESSION_ID || "ek5SW_Queen-Anya_GpUYVojVzlqNmRBeDVrUlFwanNXTmxXW_Queen-Anya_VUtbmRvY3VMeDgzenBvW_Queen-Anya_EtuYW_Queen-Anya_poc2w2SQ==",
 
     /**
      * thumbnails and medias
